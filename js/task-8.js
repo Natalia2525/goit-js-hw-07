509d1482da20b02575c3bfb5d4c90a2025187441
Natalia2525 <65954006+Natalia2525@users.noreply.github.com>
2020-11-14 15:41:30 +0200

    Add task 8

--- js/task-8.js
+++ js/task-8.js
@@ -1,27 +1,24 @@
+const inputRef = document.querySelector(`#controls input`);
+const createBtnRef = document.querySelector(`[data-action="render"]`);
+const deleteBtnRef = document.querySelector(`[data-action="destroy"]`);
+const boxesRef = document.querySelector('#boxes');
 
+const createBoxes = (amount) => {
+    const randColor = () => Math.floor(Math.random() * 255);
+    for (let i = 0, starSize = 30; i < amount; i += 1, starSize += 10) {
+        const box = document.createElement('div');
+        box.style.width = `${starSize}px`;
+        box.style.height = `${starSize}px`;
+        box.style.backgroundColor = `rgb(${randColor()},${randColor()},${randColor()})`;
+        boxesRef.append(box);
+    }
+};
+const createValue = () => createBoxes(inputRef.value);
+createBtnRef.addEventListener('click', createValue);
 
+const destroyBoxes = () => {
+  inputRef.value = 0;
+  boxesRef.innerHTML = '';
+};
 
-// Напиши скрипт создания и очистки коллекции элементов.
-// Пользователь вводит количество элементов в input и нажимает
-// кнопку Создать, после чего рендерится коллекция.При нажатии на
-// кнопку Очистить, коллекция элементов очищается.
-
-// Создай функцию createBoxes(amount), которая принимает 1 параметр
-// amount - число.Функция создает столько div, сколько указано в amount
-// и добавляет их в div#boxes.
-
-// Каждый созданный div:
-
-// Имеет случайный rgb цвет фона
-// Размеры самого первого div - 30px на 30px
-// Каждый следующий div после первого, должен быть шире и выше
-// предыдущего на 10px
-// Создай функцию destroyBoxes(), которая очищает div#boxes.
-
-// <div id="controls">
-//   <input type="number" min="0" max="100" step="1" />
-//   <button type="button" data-action="render">Создать</button>
-//   <button type="button" data-action="destroy">Очистить</button>
-// </div>
-
-// <div id="boxes"></div>
+deleteBtnRef.addEventListener('click', destroyBoxes);
